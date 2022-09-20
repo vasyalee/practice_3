5544d57115b46df8eb47b2c2b87c684a95cc5801
vasyalee <flightsho@gmail.com>
2022-09-20 22:27:33 +0300

    added task_34

---
+++ task_34/task_34.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	ifstream fin;
+	fin.open("D:\\TEST2.txt");
+	if (!fin.is_open()) {
+		"Can't open the file!";
+	}
+	else {
+		char ch;
+			while (!(fin.eof())) {
+				fin.get(ch);
+					if (ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5' || ch == '6' ||
+						ch == '7' || ch == '8' || ch == '9' || ch == '0') {
+						cout << ch;
+					}
+					else {
+						continue;
+					}
+			}
+	}
+	fin.close();
+}
